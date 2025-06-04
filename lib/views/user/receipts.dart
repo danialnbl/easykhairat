b8649636d89909a6d7a60195f57ffc6219257961
danialnbl <danialnabil0208@gmail.com>
2025-06-04 14:50:59 +0800

    up receipt

--- lib/views/user/receipts.dart
+++ lib/views/user/receipts.dart
@@ -1,14 +1,20 @@
 import 'dart:async';
-
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:badges/badges.dart' as badges;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:moon_design/moon_design.dart';
 import 'package:easykhairat/controllers/payment_controller.dart';
-import 'package:easykhairat/controllers/auth_controller.dart';
 import 'package:easykhairat/models/paymentModel.dart';
 import 'package:intl/intl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 class Receipts extends StatefulWidget {
   const Receipts({Key? key}) : super(key: key);
@@ -315,7 +321,7 @@ class _ReceiptsState extends State<Receipts> {
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start, // Add this
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     // First column - wrap this in Expanded to prevent overflow
                     Expanded(
@@ -335,7 +341,6 @@ class _ReceiptsState extends State<Receipts> {
                           ),
                           SizedBox(width: 12),
                           Expanded(
-                            // Add this to make text wrap
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
@@ -345,9 +350,10 @@ class _ReceiptsState extends State<Receipts> {
                                     fontSize: 16,
                                     fontWeight: FontWeight.bold,
                                   ),
-                                  overflow: TextOverflow.ellipsis, // Add this
-                                  maxLines: 2, // Add this
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
                                 ),
+                                SizedBox(height: 4), // Added spacing
                                 Text(
                                   formatDate(payment.paymentCreatedAt),
                                   style: TextStyle(
@@ -361,7 +367,7 @@ class _ReceiptsState extends State<Receipts> {
                         ],
                       ),
                     ),
-                    SizedBox(width: 8), // Add spacing between columns
+                    SizedBox(width: 8),
                     // Second column - payment amount
                     Container(
                       padding: EdgeInsets.symmetric(
@@ -383,18 +389,17 @@ class _ReceiptsState extends State<Receipts> {
                   ],
                 ),
                 SizedBox(height: 12),
-                Divider(),
+                Divider(height: 1), // Reduced height
+                SizedBox(height: 8), // Control spacing
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Expanded(
-                      // Add this to prevent overflow
                       child: Row(
                         children: [
                           Icon(statusIcon, color: statusColor, size: 16),
                           SizedBox(width: 4),
                           Flexible(
-                            // Add this to make text wrap if needed
                             child: Text(
                               'Pembayaran Diterima',
                               style: TextStyle(
@@ -472,32 +477,42 @@ class _ReceiptsState extends State<Receipts> {
                     ),
                   ),
                 ),
-                SizedBox(height: 32),
-                Container(
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: Column(
-                    children: [
-                      _buildDetailRow('ID Pembayaran', '#${payment.paymentId}'),
-                      Divider(),
-                      _buildDetailRow('Penerangan', payment.paymentDescription),
-                      Divider(),
-                      _buildDetailRow(
-                        'Tarikh',
-                        formatDate(payment.paymentCreatedAt),
+                SizedBox(height: 24),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: BorderRadius.circular(16),
                       ),
-                      Divider(),
-                      _buildDetailRow(
-                        'Kaedah Pembayaran',
-                        payment.paymentType ?? 'Unknown',
+                      child: Column(
+                        children: [
+                          _buildDetailRow(
+                            'ID Pembayaran',
+                            '#${payment.paymentId}',
+                          ),
+                          Divider(height: 16),
+                          _buildDetailRow(
+                            'Penerangan',
+                            payment.paymentDescription,
+                          ),
+                          Divider(height: 16),
+                          _buildDetailRow(
+                            'Tarikh',
+                            formatDate(payment.paymentCreatedAt),
+                          ),
+                          Divider(height: 16),
+                          _buildDetailRow(
+                            'Kaedah Pembayaran',
+                            payment.paymentType ?? 'Unknown',
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-                Spacer(),
+                SizedBox(height: 16),
                 Row(
                   children: [
                     Expanded(
@@ -510,15 +525,7 @@ class _ReceiptsState extends State<Receipts> {
                             borderRadius: BorderRadius.circular(12),
                           ),
                         ),
-                        onPressed: () {
-                          // Download functionality would go here
-                          Get.snackbar(
-                            'Coming Soon',
-                            'Download functionality will be added soon',
-                            backgroundColor: MoonColors.light.hit,
-                            colorText: Colors.white,
-                          );
-                        },
+                        onPressed: () => _downloadReceipt(context, payment),
                       ),
                     ),
                     SizedBox(width: 12),
@@ -534,15 +541,7 @@ class _ReceiptsState extends State<Receipts> {
                             borderRadius: BorderRadius.circular(12),
                           ),
                         ),
-                        onPressed: () {
-                          // Share functionality would go here
-                          Get.snackbar(
-                            'Coming Soon',
-                            'Share functionality will be added soon',
-                            backgroundColor: MoonColors.light.hit,
-                            colorText: Colors.white,
-                          );
-                        },
+                        onPressed: () => _shareReceipt(context, payment),
                       ),
                     ),
                   ],
@@ -577,5 +576,225 @@ class _ReceiptsState extends State<Receipts> {
         ],
       ),
     );
+  }
+
+  Future<File> _generatePdf(PaymentModel payment) async {
+    final pdf = pw.Document();
+
+    // Get a logo for the receipt
+    final ByteData logoData = await rootBundle.load(
+      'assets/images/easyKhairatLogo.png',
+    );
+    final Uint8List logoBytes = logoData.buffer.asUint8List();
+    final logoImage = pw.MemoryImage(logoBytes);
+
+    pdf.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return pw.Column(
+            crossAxisAlignment: pw.CrossAxisAlignment.start,
+            children: [
+              // Header with logo
+              pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                children: [
+                  pw.Image(logoImage, width: 60, height: 60),
+                  pw.Column(
+                    crossAxisAlignment: pw.CrossAxisAlignment.end,
+                    children: [
+                      pw.Text(
+                        'EasyKhairat',
+                        style: pw.TextStyle(
+                          fontWeight: pw.FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      pw.Text(
+                        'Resit Pembayaran',
+                        style: pw.TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              pw.SizedBox(height: 30),
+              // Receipt contents
+              pw.Container(
+                padding: pw.EdgeInsets.all(15),
+                decoration: pw.BoxDecoration(
+                  border: pw.Border.all(width: 1, color: PdfColors.grey300),
+                  borderRadius: pw.BorderRadius.circular(8),
+                ),
+                child: pw.Column(
+                  crossAxisAlignment: pw.CrossAxisAlignment.start,
+                  children: [
+                    _buildPdfRow('ID Pembayaran', '#${payment.paymentId}'),
+                    pw.Divider(),
+                    _buildPdfRow('Penerangan', payment.paymentDescription),
+                    pw.Divider(),
+                    _buildPdfRow(
+                      'Tarikh',
+                      formatDate(payment.paymentCreatedAt),
+                    ),
+                    pw.Divider(),
+                    _buildPdfRow(
+                      'Kaedah Pembayaran',
+                      payment.paymentType ?? 'Unknown',
+                    ),
+                    pw.Divider(),
+                    pw.SizedBox(height: 20),
+                    pw.Row(
+                      mainAxisAlignment: pw.MainAxisAlignment.end,
+                      children: [
+                        pw.Text(
+                          'Jumlah: ',
+                          style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                        ),
+                        pw.Text(
+                          'RM ${payment.paymentValue.toStringAsFixed(2)}',
+                          style: pw.TextStyle(
+                            fontWeight: pw.FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              pw.SizedBox(height: 40),
+              pw.Center(
+                child: pw.Text(
+                  'Terima kasih kerana membuat pembayaran.',
+                  style: pw.TextStyle(fontStyle: pw.FontStyle.italic),
+                ),
+              ),
+              pw.Spacer(),
+              pw.Center(
+                child: pw.Text(
+                  'Dokumen ini dihasilkan secara digital melalui aplikasi EasyKhairat',
+                  style: pw.TextStyle(fontSize: 10, color: PdfColors.grey700),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+
+    // Save the PDF
+    final output = await getTemporaryDirectory();
+    final String fileName = 'EasyKhairat_Receipt_${payment.paymentId}.pdf';
+    final file = File('${output.path}/$fileName');
+    await file.writeAsBytes(await pdf.save());
+
+    return file;
+  }
+
+  pw.Widget _buildPdfRow(String label, String value) {
+    return pw.Padding(
+      padding: pw.EdgeInsets.symmetric(vertical: 5),
+      child: pw.Row(
+        mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+        children: [
+          pw.Text(label, style: pw.TextStyle(color: PdfColors.grey700)),
+          pw.Text(value, style: pw.TextStyle(fontWeight: pw.FontWeight.bold)),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _downloadReceipt(
+    BuildContext context,
+    PaymentModel payment,
+  ) async {
+    try {
+      // Show loading indicator
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => Center(child: CircularProgressIndicator()),
+      );
+
+      // Generate the PDF
+      final File pdfFile = await _generatePdf(payment);
+
+      // Dismiss the loading indicator
+      Navigator.of(context).pop();
+
+      // Preview the PDF with printing package
+      await Printing.layoutPdf(
+        onLayout: (_) async => pdfFile.readAsBytes(),
+        name: 'EasyKhairat_Receipt_${payment.paymentId}.pdf',
+        format: PdfPageFormat.a4,
+      );
+
+      Get.snackbar(
+        'Berjaya',
+        'Resit telah dimuat turun',
+        backgroundColor: Colors.green,
+        colorText: Colors.white,
+        duration: Duration(seconds: 2),
+      );
+    } catch (e) {
+      print('Error generating PDF: $e');
+
+      // Dismiss the loading indicator if it's still showing
+      Navigator.of(context, rootNavigator: true).pop();
+
+      Get.snackbar(
+        'Error',
+        'Gagal menjana resit: ${e.toString()}',
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+    }
+  }
+
+  Future<void> _shareReceipt(BuildContext context, PaymentModel payment) async {
+    try {
+      // Show loading indicator
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => Center(child: CircularProgressIndicator()),
+      );
+
+      // Generate the PDF
+      final File pdfFile = await _generatePdf(payment);
+
+      // Dismiss the loading indicator
+      Navigator.of(context).pop();
+
+      // Share the file
+      final result = await Share.shareXFiles(
+        [XFile(pdfFile.path)],
+        text: 'Resit Pembayaran EasyKhairat',
+        subject: 'Resit #${payment.paymentId}',
+      );
+
+      if (result.status == ShareResultStatus.success) {
+        Get.snackbar(
+          'Berjaya',
+          'Resit telah dikongsi',
+          backgroundColor: Colors.green,
+          colorText: Colors.white,
+          duration: Duration(seconds: 2),
+        );
+      }
+    } catch (e) {
+      print('Error sharing PDF: $e');
+
+      // Dismiss the loading indicator if it's still showing
+      Navigator.of(context, rootNavigator: true).pop();
+
+      Get.snackbar(
+        'Error',
+        'Gagal berkongsi resit: ${e.toString()}',
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+    }
   }
 }
